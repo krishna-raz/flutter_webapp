78e5350f0eff24f57c2d7215c9a971051aaffa81
krishna-raz <krishnaraj143007@gmail.com>
2023-09-29 15:10:41 +0530

    add some

---
+++ android/app/src/main/kotlin/com/example/wezire/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wezire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Website App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime? currentBackPressTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (currentBackPressTime == null ||
+            DateTime.now().difference(currentBackPressTime!) >
+                Duration(seconds: 2)) {
+          currentBackPressTime = DateTime.now();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Press back again to exit the app"),
+            ),
+          );
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+         
+       
+        body: WebView(
+          initialUrl: 'https://shop.bcepatna.live/', // Replace with your website URL
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
