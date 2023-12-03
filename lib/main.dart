e69ede9587bf9e71f9e4a51fe02c88ef82009fe3
krishna-raz <krishnaraj143007@gmail.com>
2023-12-03 12:29:14 +0530

    add back button

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _webViewController;
   DateTime? currentBackPressTime;
 
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
+  Future<bool> _onWillPop() async {
+    if (_webViewController != null) {
+      if (await _webViewController.canGoBack()) {
+        _webViewController.goBack();
+        return false; // Do not exit the app
+      } else {
         if (currentBackPressTime == null ||
             DateTime.now().difference(currentBackPressTime!) >
                 Duration(seconds: 2)) {
@@ -37,16 +40,46 @@ class _MyHomePageState extends State<MyHomePage> {
               content: Text("Press back again to exit the app"),
             ),
           );
-          return false;
+          return false; // Do not exit the app
+        } else {
+          return await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Confirm Exit'),
+              content: Text('Do you want to exit the app?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: Text('No'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: Text('Yes'),
+                ),
+              ],
+            ),
+          ) ?? false;
         }
-        return true;
-      },
-      child: Scaffold(
-         
-       
-        body: WebView(
-          initialUrl: 'https://shop.bcepatna.live/', // Replace with your website URL
+      }
+    }
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('WebView Example'),
+      // ),
+      body: WillPopScope(
+        onWillPop: _onWillPop,
+        child: WebView(
+          initialUrl:
+              'https://www.enableds.com/products/sticky/v53/index.html', // Replace with your website URL
           javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (controller) {
+            _webViewController = controller;
+          },
         ),
       ),
     );
