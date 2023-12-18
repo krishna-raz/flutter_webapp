3f83ea44df8ea92cfda324ff9002c64966c1874e
Krishna Raj <78550375+krishna-raz@users.noreply.github.com>
2023-12-18 21:56:02 +0530

    Add connectivity check and error handling in WebView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -25,41 +27,68 @@ class _MyHomePageState extends State<MyHomePage> {
   late WebViewController _webViewController;
   DateTime? currentBackPressTime;
 
+  bool hasConnectivityError = false;
+
+  Future<void> _checkConnectivity() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        setState(() {
+          hasConnectivityError = false;
+        });
+      }
+    } on SocketException catch (_) {
+      setState(() {
+        hasConnectivityError = true;
+      });
+    }
+  }
+
   Future<bool> _onWillPop() async {
-    if (_webViewController != null) {
-      if (await _webViewController.canGoBack()) {
-        _webViewController.goBack();
+    await _checkConnectivity();
+
+    if (hasConnectivityError) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+       const SnackBar(
+          content: Text("Please check your internet connection."),
+        ),
+      );
+      return false;
+    }
+
+    if (await _webViewController.canGoBack()) {
+      _webViewController.goBack();
+      return false; // Do not exit the app
+    } else {
+      if (currentBackPressTime == null ||
+          DateTime.now().difference(currentBackPressTime!) >
+              Duration(seconds: 2)) {
+        currentBackPressTime = DateTime.now();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Press back again to exit the app"),
+          ),
+        );
         return false; // Do not exit the app
       } else {
-        if (currentBackPressTime == null ||
-            DateTime.now().difference(currentBackPressTime!) >
-                Duration(seconds: 2)) {
-          currentBackPressTime = DateTime.now();
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text("Press back again to exit the app"),
-            ),
-          );
-          return false; // Do not exit the app
-        } else {
-          return await showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text('Confirm Exit'),
-              content: Text('Do you want to exit the app?'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Navigator.of(context).pop(false),
-                  child: Text('No'),
-                ),
-                TextButton(
-                  onPressed: () => Navigator.of(context).pop(true),
-                  child: Text('Yes'),
-                ),
-              ],
-            ),
-          ) ?? false;
-        }
+        return await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Confirm Exit'),
+            content: Text('Do you want to exit the app?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        ) ?? false;
       }
     }
     return true;
@@ -68,18 +97,25 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('WebView Example'),
-      // ),
       body: WillPopScope(
         onWillPop: _onWillPop,
-        child: WebView(
-          initialUrl:
-              'https://www.enableds.com/products/sticky/v53/index.html', // Replace with your website URL
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (controller) {
-            _webViewController = controller;
-          },
+        child: SafeArea(
+          child: WebView(
+            initialUrl:
+                'https://bcepatna.live/KnitKraft/', // Replace with your website URL
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (controller) {
+              _webViewController = controller;
+            },
+            onWebResourceError: (WebResourceError error) {
+              if (error.errorCode == 101) {
+                // 101 indicates connection error
+                setState(() {
+                  hasConnectivityError = true;
+                });
+              }
+            },
+          ),
         ),
       ),
     );
